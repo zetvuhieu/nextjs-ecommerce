89277d8b80e0b79e37cafcf2dbd68a5eb7707f25
Nguyen Vu Hieu <2405vuhieuzet@gmail.com>
2025-03-30 16:17:01 +0700

    config variables in header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-600">
-        {" "}
-        {/* Sử dụng màu đỏ */}
-        Welcome to Next.js + Tailwind TEST
+        Welcome to Next.js + Tailwind
       </h1>
       <p className="mt-2 text-gray-700">Tailwind is successfully set up! 🎉</p>
     </div>
